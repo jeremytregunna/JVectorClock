508b3a842d302b0d1d5255af63ed64d824df655d
Jeremy Tregunna <jeremy@tregunna.ca>
2013-04-06 20:06:48 -0600

    adds static ios lib target

---
+++ JVectorClock-iOS/JVectorClock_iOS.h
@@ -0,0 +1,13 @@
+//
+//  JVectorClock_iOS.h
+//  JVectorClock-iOS
+//
+//  Created by Jeremy Tregunna on 2013-04-06.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JVectorClock_iOS : NSObject
+
+@end
---
+++ JVectorClock-iOS/JVectorClock_iOS.m
@@ -0,0 +1,13 @@
+//
+//  JVectorClock_iOS.m
+//  JVectorClock-iOS
+//
+//  Created by Jeremy Tregunna on 2013-04-06.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import "JVectorClock_iOS.h"
+
+@implementation JVectorClock_iOS
+
+@end
